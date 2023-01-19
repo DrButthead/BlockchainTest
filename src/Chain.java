26bdb9a9b9cba459cb59f9460bef6bfa4ba92a9f
olivero <oliver.olding@hotmail.com>
2023-01-19 23:55:39 +0000

    Added validity check

--- src/Chain.java
+++ src/Chain.java
@@ -21,6 +21,30 @@ public class Chain {
         blockChain.add(new Block(data, previousHash));
     }
 
+    public Boolean isChainValid() {
+        Block previousBlock;
+        Block currentBlock;
+
+        // Loop through the whole chain to check the hashes are correct
+        for (int i = 1; i < blockChain.size(); i++) {
+            previousBlock = blockChain.get(i-1);
+            currentBlock = blockChain.get(i);
+
+            // Compare the registered hash and the calculated hash
+            if (!currentBlock.hash.equals(currentBlock.calculateHash())) {
+                System.out.println("Current hashes do not match!");
+                return false;
+            }
+
+            // Compare previous registered hash and the calculated hash
+            if (!previousBlock.hash.equals(currentBlock.previousHash)) {
+                System.out.println("Previous hashes to not match!");
+                return false;
+            }
+        }
+        return true;
+    }
+
 
     @Override
     public String toString() {
--- test/ChainTest.java
+++ test/ChainTest.java
@@ -1,5 +1,7 @@
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 class ChainTest {
 
     @Test
@@ -12,5 +14,25 @@ class ChainTest {
 
         System.out.println(blockChain);
         System.out.println(blockChain.toJsonString());
+    }
+
+    @Test
+    void isChainValid() {
+        Chain blockChain = new Chain();
+
+        assertTrue(blockChain.isChainValid());
+
+        blockChain.add("Number 1");
+        blockChain.add("Number 2");
+        blockChain.add("Number 3");
+        blockChain.add("Number 4");
+
+        assertTrue(blockChain.isChainValid());
+
+        Block changeMe = blockChain.blockChain.get(1);
+        changeMe = new Block("Number 2", changeMe.previousHash); // timestamp should be different
+        blockChain.blockChain.set(1, changeMe);
+
+        assertFalse(blockChain.isChainValid());
     }
 }
